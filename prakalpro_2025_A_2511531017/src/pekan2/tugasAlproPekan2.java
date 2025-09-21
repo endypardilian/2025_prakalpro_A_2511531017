9d8cff509f0ebc66074226a6a71b7fc3617a07f7
Endy <Endy@DESKTOP-BLNNKHQ>
2025-09-21 11:14:27 +0700

    TugasAlproPekan2

---
+++ prakalpro_2025_A_2511531017/src/pekan2/tugasAlproPekan2.java
@@ -0,0 +1,17 @@
+package pekan2;
+
+public class tugasAlproPekan2 {
+    public static void main(String[] args) {
+       
+        int umur = 20;                
+        float suhuTubuh = 37.8f;      
+        char golonganDarah = 'O';     
+        boolean statusDemam = true;   
+
+        System.out.println("=== STATUS KESEHATAN ===");
+        System.out.println("Umur           : " + umur + " tahun");
+        System.out.println("Suhu Tubuh     : " + suhuTubuh + " °C");
+        System.out.println("Golongan Darah : " + golonganDarah);
+        System.out.println("Sedang Demam?  : " + statusDemam);
+    }
+}
