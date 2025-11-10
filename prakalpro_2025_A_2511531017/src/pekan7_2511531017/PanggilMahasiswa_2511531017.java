e531d68ecbe652d91e4bf959aba04054544150d6
endypardilian <endy.npd@gmail.com>
2025-11-10 14:46:21 +0700

    Praktikum7_KelasA_2511531017

---
+++ prakalpro_2025_A_2511531017/src/pekan7_2511531017/PanggilMahasiswa_2511531017.java
@@ -0,0 +1,13 @@
+package pekan7_2511531017;
+
+public class PanggilMahasiswa_2511531017 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511531017 a= new Mahasiswa_2511531017();
+		a.setNim(23532);
+		a.setnama("Endy");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
---
+++ prakalpro_2025_A_2511531017/src/pekan7_2511531017/String2_2511531017.java
@@ -0,0 +1,24 @@
+package pekan7_2511531017;
+
+public class String2_2511531017 {
+
+	public static void main(String[] args) {
+		String firstName = "Endy";
+		String lastName = "Pardilian";
+		String txt1 = "Dosen\"intelektual\"kampus";
+		System.out.println("Nama Lengkap: "+ firstName+" "+lastName);
+		System.out.println("Nama Lenglap: "+firstName.concat(lastName));
+		System.out.println(txt1);
+		int x=10;
+		int y=20;
+		int z= x + y;
+		System.out.println("x+y= "+z);
+		String a= "10";
+		String b= "20";
+		String c= a + b;
+		System.out.println("String a + string b = "+c);
+		String v = a + y;
+		System.out.println("String a + string y = "+v);
+	}
+
+}
